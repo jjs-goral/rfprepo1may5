2601b83f0b88182e37178784aaebc69dd1b69105
John Smutniak <jjsdunkirk@Johns-MBP-2.attlocal.net>
2025-05-06 12:24:43 -0500

    Re-add cleaned projectId route files

---
+++ src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ProjectContent from '@/components/project/ProjectContent';
+
+export default function ProjectPage({ params }: { params: { id: string } }) {
+  return <ProjectContent />;
+}
